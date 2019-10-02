2d136a0cfd917f3442e923a90e95cce8f82051f1
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 19:48:23 -0400

    calculate a large prime number instead of executing electron

--- parent.js
+++ parent.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 const proc1 = spawn('node', ['child.js'], { stdio: ['ignore', 'pipe', 'pipe' ]});
-const proc2 = spawn('npx', ['electron', '--version'], { stdio: ['ignore', 'pipe', 'pipe' ]});
+const proc2 = spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]});
 
 proc1.stdout.pipe(process.stdout);
 proc1.stderr.pipe(process.stderr);
