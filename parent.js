3180736a959b60b4436c695b5c3df748d5132a48
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 17:45:16 -0400

    using correct file

--- parent.js
+++ parent.js
@@ -1,4 +1,4 @@
 const { spawn } = require('child_process');
 
-spawn('node', ['debug.js'], { stdio: 'inherit' });
+spawn('node', ['child.js'], { stdio: 'inherit' });
 spawn('npx', ['electron', '--version'], { stdio: 'inherit' });
