1bd692b4ef8e5a72f019232c5aaf4e63a1168e78
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 17:58:30 -0400

    piping streams just in case

--- parent.js
+++ parent.js
@@ -1,4 +1,9 @@
 const { spawn } = require('child_process');
 
-spawn('node', ['child.js'], { stdio: 'inherit' });
-spawn('npx', ['electron', '--version'], { stdio: 'inherit' });
+const proc1 = spawn('node', ['child.js'], { stdio: ['ignore', 'pipe', 'pipe' ]});
+const proc2 = spawn('npx', ['electron', '--version'], { stdio: ['ignore', 'pipe', 'pipe' ]});
+
+proc1.stdout.pipe(process.stdout);
+proc1.stderr.pipe(process.stderr);
+proc2.stdout.pipe(process.stdout);
+proc2.stderr.pipe(process.stderr);
