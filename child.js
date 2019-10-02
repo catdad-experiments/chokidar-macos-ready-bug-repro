cbcd4a5dfbcb9d2a2d425c52ec6bf467d627bd0c
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 17:38:20 -0400

    adding initial implementation files from gist
    https://gist.github.com/catdad/d7f0bff7b47d0796f994174aa078719c

---
+++ child.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const chokidar = require('chokidar');
+
+const root = path.resolve('.');
+
+const watcher = chokidar.watch('.', {
+  cwd: root,
+  ignored: [
+    /(^|[/\\])\../, // Dotfiles
+    'node_modules',
+    '**/*.map'
+  ]
+});
+
+watcher.on('add', f => console.log('add', f));
+watcher.on('ready', () => console.log('ready'));
+
+// exit after 10 seconds... it should definitely be enough time
+setTimeout(() => {
+  process.exit();
+}, 10000);
