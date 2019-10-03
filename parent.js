b73bfebc76e816a89bf09374d74cd9c42863f65c
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 20:06:25 -0400

    calculating primes 3 times, leaving 1 free thread on GitHub actions

--- parent.js
+++ parent.js
@@ -1,9 +1,11 @@
 const { spawn } = require('child_process');
 
-const proc1 = spawn('node', ['child.js'], { stdio: ['ignore', 'pipe', 'pipe' ]});
-const proc2 = spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]});
-
-proc1.stdout.pipe(process.stdout);
-proc1.stderr.pipe(process.stderr);
-proc2.stdout.pipe(process.stdout);
-proc2.stderr.pipe(process.stderr);
+[
+  spawn('node', ['child.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+].forEach(proc => {
+  proc.stdout.pipe(process.stdout);
+  proc.stderr.pipe(process.stderr);
+});
