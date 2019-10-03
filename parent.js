9d808d7fa3cff8f0a9606e1f081f02f6e1af5482
Kiril Vatev <vatev.1@gmail.com>
2019-10-02 20:33:08 -0400

    going back to just two tasks, this time executing `npm -v` in parallel

--- parent.js
+++ parent.js
@@ -2,9 +2,8 @@ const { spawn } = require('child_process');
 
 [
   spawn('node', ['child.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
-  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
-  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
-  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+//  spawn('node', ['prime.js'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
+  spawn('npm', ['-v'], { stdio: ['ignore', 'pipe', 'pipe' ]}),
 ].forEach(proc => {
   proc.stdout.pipe(process.stdout);
   proc.stderr.pipe(process.stderr);
